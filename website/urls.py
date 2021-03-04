d8a2b1416cfd39f35994fbcc1715bffe523563c2
KaiLascheit000 <2439815l@student.gla.ac.uk>
2021-03-04 14:35:53 +0100

    created views

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from website import views
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', views.index, name = 'index')
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# home page
+def index(request):
+    return HttpResponse("This will be the Homepage")
+
+# planet page
+def planets(request):
+    return HttpResponse("This will be a planet page")
+
+# profile page
+def profile(request):
+    return HttpResponse("This will be the profile page")
+
+# single post page
+def post(request):
+    return HttpResponse("This will be a post page")
+
+# post creation page
+def create_post(request):
+    return HttpResponse("This will be the page to create posts")
+
+# feed page
+def feed(request):
+    return HttpResponse("This will be the feed page")
+
+# authentication page
+def authenticate(request):
+    return HttpResponse("This will be the page for logging in and signing up")
