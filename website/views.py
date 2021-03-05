30d21af4daf0dcc4361c5f33b3b3441926267124
KaiLascheit000 <2439815l@student.gla.ac.uk>
2021-03-05 14:00:04 +0100

    added links between views

--- website/views.py
+++ website/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # home page
 def index(request):
-    return HttpResponse("This will be the Homepage")
+    planets = "<a href='planet/'>planet</a>"
+    profile = "<a href='profile/'>profile</a>"
+    create_post = "<a href='create_post/'>create_post</a>"
+    authenticate = "<a href='authenticate/'>authenticate</a>"
+    return HttpResponse("This will be the Homepage " + planets + " " + profile + " " + create_post + " " + authenticate)
 
 # planet page
 def planets(request):
-    return HttpResponse("This will be a planet page")
+    index = "<a href='index/'>Homepage</a>"
+    profile = "<a href='profile/'>profile</a>"
+    create_post = "<a href='create_post/'>create_post</a>"
+    authenticate = "<a href='authenticate/'>authenticate</a>"
+    return HttpResponse("This will be a planet page " + index + " " + profile + " " + create_post + " " + authenticate)
 
 # profile page
 def profile(request):
-    return HttpResponse("This will be the profile page")
+    index = "<a href='index/'>Homepage</a>"
+    planets = "<a href='planet/'>planet</a>"
+    create_post = "<a href='create_post/'>create_post</a>"
+    authenticate = "<a href='authenticate/'>authenticate</a>"
+    return HttpResponse("This will be the profile page " + index + " " + planets + " " + create_post + " " + authenticate)
 
 # single post page
 def post(request):
+    
     return HttpResponse("This will be a post page")
 
 # post creation page
 def create_post(request):
-    return HttpResponse("This will be the page to create posts")
+    index = "<a href='index/'>Homepage</a>"
+    planets = "<a href='planet/'>planet</a>"
+    profile = "<a href='profile/'>profile</a>"
+    authenticate = "<a href='authenticate/'>authenticate</a>"
+    return HttpResponse("This will be the page to create posts " + index + " " + planets + " " + profile + " " + authenticate)
 
 # feed page
 def feed(request):
@@ -27,4 +46,8 @@ def feed(request):
 
 # authentication page
 def authenticate(request):
-    return HttpResponse("This will be the page for logging in and signing up")
+    index = "<a href='index/'>Homepage</a>"
+    planets = "<a href='planet/'>planet</a>"
+    profile = "<a href='profile/'>profile</a>"
+    create_post = "<a href='create_post/'>create post</a>"
+    return HttpResponse("This will be the page for logging in and signing up " + index + " " + planets + " " + profile + " " + create_post)
