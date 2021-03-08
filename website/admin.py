a40475873b3bf6e8a94356530fc8b1092319f10c
brownben <9870007+brownben@users.noreply.github.com>
2021-03-08 13:12:46 +0000

    feat: add basic models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Planet, Post, Reaction, Comment, UserProfile
 
-# Register your models here.
+
+admin.site.register(Planet)
+admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Reaction)
+admin.site.register(Comment)
