725900af3d3be83a15bf38c8cc30771ca770aac5
brownben <9870007+brownben@users.noreply.github.com>
2021-03-08 13:12:46 +0000

    feat: add basic models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Planet(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField()
+    description = models.TextField()
+    data = models.TextField()
+    image = models.ImageField(upload_to="profile_images", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    planet = models.ForeignKey(Planet, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+    body = models.TextField()
+    slug = models.SlugField()
+    time_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.heading
+
+
+class Reaction(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)
+    time_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.id
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_created = models.DateTimeField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.id
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to="profile_images", blank=True)
+
+    def __str__(self):
+        return self.user.username
