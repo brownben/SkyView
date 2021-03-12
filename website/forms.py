ace338d59d6f28da8e129ceea80d0445aabd7fef
Eva Kupcova <2479253k@student.gla.ac.uk>
2021-03-12 14:09:54 +0000

    userauth

---
+++ website/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django import forms
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
--- website/models.py
+++ website/models.py
@@ -48,6 +48,7 @@ class Comment(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
     picture = models.ImageField(upload_to="profile_images", blank=True)
 
     def __str__(self):
