062940020bd42e7c58566305b1bb094c5765b270
brownben <9870007+brownben@users.noreply.github.com>
2021-03-15 14:57:23 +0000

    fix: NASA Image Can Be YouTube Video

--- static/scripts/nasaPictureOfTheDay.js
+++ static/scripts/nasaPictureOfTheDay.js
@@ -1,9 +1,39 @@
-const nasaImage = document.getElementById('nasaPicture')
-fetch(
-  'https://api.nasa.gov/planetary/apod?api_key=PI5Fhlc0LjXWmwlekt1BpxTSfixkveFg8NQGI8FA'
-)
-  .then((data) => data.json())
-  .then(({ url, title }) => {
-    nasaImage.setAttribute('src', url)
-    nasaImage.setAttribute('alt', `The NASA Picture of the Day - "${title}"`)
-  })
+const createIframe = (url) => {
+  const iframe = document.createElement('iframe')
+  iframe.setAttribute('src', url + ';modestbranding=1')
+  iframe.setAttribute('frameborder', '0')
+  iframe.setAttribute('allowfullscreen', true)
+  iframe.setAttribute('width', '560')
+  iframe.setAttribute('height', '315')
+  iframe.setAttribute('id', 'nasaPicture')
+  return iframe
+}
+
+const createImage = (url, title) => {
+  const image = document.createElement('img')
+  image.setAttribute('src', url)
+  image.setAttribute('alt', `The NASA Picture of the Day - "${title}"`)
+  image.setAttribute('id', 'nasaPicture')
+  return image
+}
+
+const createCorrectContainer = (url, title) => {
+  if (url.includes('youtube.com')) return createIframe(url)
+  else return createImage(url, title)
+}
+
+const getNASAData = async () => {
+  const data = await fetch(
+    'https://api.nasa.gov/planetary/apod?api_key=PI5Fhlc0LjXWmwlekt1BpxTSfixkveFg8NQGI8FA'
+  )
+  return data.json()
+}
+
+const insertPictureOfTheDay = async () => {
+  const nasaPictureContainer = document.getElementById('nasaPictureContainer')
+  const { url, title } = await getNASAData()
+  const container = createCorrectContainer(url, title)
+  nasaPictureContainer.appendChild(container)
+}
+
+insertPictureOfTheDay()
